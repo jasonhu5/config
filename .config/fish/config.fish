46848ba8b9c4cb8d5a0dea152e3e76d4a3706368
Jason Hu <jasonhu515@gmail.com>
2021-06-01 23:12:31 +0200

    adjust tide options

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,6 +10,11 @@ bind \ch forward-char
 # make fzf search hidden files too
 set -U fzf_fd_opts --hidden --exclude .git
 
+# set tide prompts
+set -g tide_left_prompt_items status newline pwd git virtual_env
+set -g tide_right_prompt_items
+set -g tide_pwd_truncate_margin 1000000000
+
 # aliases
 alias sp="source ~/.config/fish/config.fish"
 alias b="cd .."
@@ -21,3 +26,6 @@ alias ga="git add"
 alias gc="git commit -m"
 alias gp="git pull"
 alias gd="git diff"
+
+# bolt config
+set -U BOLT_DISABLE_ANALYTICS true
