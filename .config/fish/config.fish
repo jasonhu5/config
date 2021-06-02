7b0be546f8f604b0de010377f5978073639b9bd3
Jason Hu <jasonhu515@gmail.com>
2021-06-02 15:23:18 +0200

    add context in fish prompt, add ~/.local/bin to PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,9 +11,12 @@ bind \ch forward-char
 set -U fzf_fd_opts --hidden --exclude .git
 
 # set tide prompts
-set -g tide_left_prompt_items status newline pwd git virtual_env
+set -g tide_left_prompt_items status newline context pwd git virtual_env
 set -g tide_right_prompt_items
 set -g tide_pwd_truncate_margin 1000000000
+
+# add to PATH ~/.local/bin
+set PATH ~/.local/bin $PATH
 
 # aliases
 alias sp="source ~/.config/fish/config.fish"
@@ -29,3 +32,5 @@ alias gd="git diff"
 
 # bolt config
 set -U BOLT_DISABLE_ANALYTICS true
+
+
